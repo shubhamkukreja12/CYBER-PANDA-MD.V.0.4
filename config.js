f3ec536cd3d1eb60e68f97f3a5bcdfb117ea1861
shubhamkukreja12 <158502270+shubhamkukreja12@users.noreply.github.com>
2024-02-02 13:32:50 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
+global.Owner = ["+923332114332", "+923332114332"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+923332114332", "+923332114332"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+923332114332"];
+global.OwnerName = "Shubhqm";
+global.BotName = "Icebot";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
 global.author = "SACHITH";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
